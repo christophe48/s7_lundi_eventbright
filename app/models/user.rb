8952a9eeb37588e60d621de8fb92196c2408f77f
Christophe BONNEFOY <christophe48bonnefoy@gmail.com>
2019-11-04 16:01:55 +0100

    Welcome mailer done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ApplicationRecord
+  after_create :welcome_send
+
+  def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
+
+  
   has_many :attendances
   has_many :events, through: :attendance
 
   validates :email, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "L'email doit être renseigné au bon format (email@eamil.email) !"},
             uniqueness: true
+
+
 end
